4ab2b099ebd8299351ff9433a08dc36219fe240a
Ryan Bass <theryanbass@gmail.com>
2022-04-29 10:12:13 -0700

    add initial project dependencies/code

---
+++ upc-project/lift.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const nunjucks = require('nunjucks');
+
+const env = nunjucks.configure('', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.static('files'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+env.express(app);
+
+app.get('/', (req, res) => {
+  res.send('Test.')
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+})
