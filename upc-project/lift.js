d05208620f47374b4f2a30de14fdcf1d8f5b44ca
Ryan Bass <theryanbass@gmail.com>
2022-04-29 17:49:42 -0700

    remove old code for express

--- upc-project/lift.js
+++ upc-project/lift.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const nunjucks = require('nunjucks');
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// const nunjucks = require('nunjucks');
 
-const env = nunjucks.configure('', {
-  autoescape: true,
-  express: app,
-  watch: true
-})
+// const env = nunjucks.configure('', {
+//   autoescape: true,
+//   express: app,
+//   watch: true
+// })
 
-app.use(express.static('files'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(express.static('files'));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
-env.express(app);
+// env.express(app);
 
-app.get('/', (req, res) => {
-  res.render('index.html', {})
-});
+// app.get('/', (req, res) => {
+//   res.render('index.html', {})
+// });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}.`);
-})
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}.`);
+// })
