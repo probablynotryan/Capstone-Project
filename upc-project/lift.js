33d6e2a8a97e7cc546b23676a5759807869bfa17
Ryan Bass <theryanbass@gmail.com>
2022-05-15 15:57:00 -0700

    test session checking

--- upc-project/lift.js
+++ upc-project/lift.js
@@ -4,33 +4,14 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const MySQLStore = require('express-mysql-session')(session);
-
-app.use(cookieParser());
-
+const app = express();
+const port = 3000;
 const myUserName = 'testUser';
 const myPassword = 'testPassword';
 
 
-
-// const options = {
-//   databaseInformation/Login
-// }
-
-// const sessionStore = new MySQLStore(options);
-
-app.use(session({
-  key: "look_at_all_them_chickens",
-  secret: "the_cake_is_on_fire",
-  store: sessionStore,
-  cookie: {maxAge: 84600000},
-  resave: false,
-  saveUninitialized: false
-}))
-
-const app = express();
-const port = 3000;
 app.use(express.static('static'));
-
+app.use(cookieParser());
 const env = nunjucks.configure('views', {
   autoescape: true,
   express: app,
@@ -39,13 +20,28 @@ const env = nunjucks.configure('views', {
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-env.express(app);
+// const options = {
+//   databaseInformation/Login
+// }
+
+// const sessionStore = new MySQLStore(options);
+
+app.use(session({
+  // key: "look_at_all_them_chickens",
+  secret: "the_cake_is_on_fire",
+  // store: sessionStore,
+  cookie: {maxAge: 84600000},
+  resave: false,
+  saveUninitialized: false
+}))
+
+// env.express(app);
 
 app.get('/', (req, res) => {
-  if (req.session){
-    res.render('index.njk', 'layout.njk')
+  if (req.session.key){
+    res.render('index.njk', {layout: 'layout.njk'})
   } else {
-  res.render('login.njk', data);
+  res.render('login.njk', {layout: 'layout.njk'});
   }
 });
 
