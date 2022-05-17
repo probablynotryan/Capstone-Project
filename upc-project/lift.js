4ae3d3f7be33b849d671bae647f9b55a7f8ce8ca
Ryan Bass <theryanbass@gmail.com>
2022-05-17 13:28:04 -0700

    fiddle with button styling

--- upc-project/lift.js
+++ upc-project/lift.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const mysql = require('mysql');
 const MySQLStore = require('express-mysql-session')(session);
-const app = express();
 const config = require('./config/default');
+const app = express();
 const port = 3000;
 
 const db = mysql.createConnection(config.db);
@@ -21,14 +22,8 @@ const env = nunjucks.configure('views', {
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// const options = {
-//   databaseInformation/Login
-// }
-
-// const sessionStore = new MySQLStore(options);
-
 app.use(session({
-  // key: "look_at_all_them_chickens",
+  key: "look_at_all_them_chickens",
   secret: "the_cake_is_on_fire",
   // store: sessionStore,
   cookie: {maxAge: 84600000},
@@ -36,11 +31,9 @@ app.use(session({
   saveUninitialized: false
 }))
 
-const testNum = "012345678910";
-
-db.query(`SELECT * FROM upc_db.upcs WHERE upc_value = ${testNum}`, function (err, result, fields) {
+db.query(`SELECT * FROM upc_db.upcs`, function (err, result, fields) {
   if (err) throw err;
-  result.map(r => console.log(r))
+  result.map(r => console.log(r.upc_value))
   });
 ;
 
@@ -53,10 +46,19 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  req.session.key = 'logged-in';
+  console.log(req.body.userName);
+  req.session.key = req.body.login;
   res.render('index.njk', {layout: 'layout.njk'});
 })
 
+app.post('/scan-try', (req, res) => {
+  db.query(`SELECT * FROM upc_db.upcs WHERE upc_value = ${req.body.scan_value}`, function (err, result, fields) {
+    if (err) throw err;
+    if (result) {
+    result.map(r => console.log(r.upc_value));
+    } 
+  });
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
